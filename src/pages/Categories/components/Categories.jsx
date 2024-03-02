79cad88c11ef8c92ecb251a0a1206bf5e9a4ffd7
NadaAsadk <nadaasad2003@gmail.com>
2024-03-02 22:42:23 +0200

    no swiper

--- src/pages/Categories/components/Categories.jsx
+++ src/pages/Categories/components/Categories.jsx
@@ -31,104 +31,18 @@ export default function Categories() {
   if (loader) {
     return <Loader />
   }
- 
-  const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: false,
-    slidesPerView: 5,
-
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-      clickable: true,
-    },
-
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-    slidesPerView: 1,
-    spaceBetween: 10,
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      },
-      480: {
-        slidesPerView: 3,
-        spaceBetween: 30
-      },
-      640: {
-        slidesPerView: 4,
-        spaceBetween: 40
-      },
-      1200: {
-        slidesPerView: 5,
-        spaceBetween: 50
-      }
-    }
-  });
 
 
   return (
-    <>
-    
-      <swiper 
-      direction= {'horizontal'}
-      loop= {false}
-  
-      pagination= {{
-        type: 'progressbar',
-        clickable: true,
-      }}
-  
-      navigation= {{
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-        clickable: true,
-      }}
-  
-      slidesPerView= {4}
-      spaceBetween= {40}
-      breakpoints= {{
-        320: {
-          slidesPerView: 2,
-          spaceBetween: 20
-        },
-        480: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        },
-        640: {
-          slidesPerView: 4,
-          spaceBetween: 40
-        },
-        1200: {
-          slidesPerView: 5,
-          spaceBetween: 50
-        }
-      }}
-      className={'swiper'}>
-        <div className="swiper-wrapper">
-          {categories.map(category =>
-            <div className="swiper-slide">
-              <img src={category.image.secure_url} alt={category.name} />
-              <p>{category.name}</p>
-            </div>
-
-          )}
-
-        </div>
-        <div className="swiper-pagination" />
-        <div className="swiper-button-prev" />
-        <div className="swiper-button-next" />
-      </swiper>
-
-    </>
-
+     <div className="categories">
+          {
+            categories.map(category =>
+              <div className="category"> 
+                  <img src={category.image.secure_url} />
+                  <p>{category.name}</p>
+               </div>
+            )
+          }
+     </div>
   );
 }
