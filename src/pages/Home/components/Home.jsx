0f8a9973b0f52881c51276c4f5ce3e9eadac7131
NadaAsadk <nadaasad2003@gmail.com>
2024-03-09 01:53:29 +0200

    add footer

--- src/pages/Home/components/Home.jsx
+++ src/pages/Home/components/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import image from './image.png'
+import image from './1.png'
 import styles from './Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.home}>
-      <img src={image}className={styles.images}/>
+      <div className={styles.home_right}>
+        <h1 className={styles.title}>Welcome to your favorite online shopping site</h1>
+      </div>
+      <div className={styles.home_left}>
+        <img src={image} className={styles.images} />
+
+      </div>
     </div>
   )
 }
