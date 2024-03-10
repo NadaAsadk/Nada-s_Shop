03331365cec1a8b4a021df17c71ee8edf983c216
NadaAsadk <nadaasad2003@gmail.com>
2024-03-10 03:22:48 +0200

    updates

--- src/pages/Home/components/Home.jsx
+++ src/pages/Home/components/Home.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import image from './1.png'
+import image from './image.jpg'
 import styles from './Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.home}>
       <div className={styles.home_right}>
-        <h1 className={styles.title}>Welcome to your favorite online shopping site</h1>
       </div>
       <div className={styles.home_left}>
         <img src={image} className={styles.images} />
