e1f72ea8b25d7c3661958f6a68f919c7ed6e2825
NadaAsadk <nadaasad2003@gmail.com>
2024-03-28 23:13:50 +0200

    add warning

--- src/components/ProtectedRoutes/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom';
+import { Bounce, toast } from 'react-toastify';
 
 export default function ProtectedRoutes({children}) {
     const token = localStorage.getItem('userToken');
     if(!token){
+      toast.warn('Confirm your login Info', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+        });
         return <Navigate to='/Signin' replace/>
     }
 
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,6 @@ import axios from 'axios'
 axios.defaults.baseURL = import.meta.env.VITE_API
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <App />,
 )
